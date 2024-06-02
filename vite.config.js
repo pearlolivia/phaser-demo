7c368ce6d7a37dc0cefa7deb8f296f4ecf68f021
pearl.vernon-howe <pearl.vernon-howe@Twin10-L186.corp.twinkl.com>
2024-06-02 18:52:35 +0100

    retry build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import path from 'path';
 
 export default defineConfig({
     base: './',
     build: {
         outDir: './docs',
+    },
+    resolve: {
+        alias: {
+            'root': path.resolve(__dirname, './'),
+            'assets': path.resolve(__dirname, './assets'),
+            'scenes': path.resolve(__dirname, './scenes'),
+        },
     },
 });
